0622e51335a97c28e96fd8b579a806ca0834aefe
Katey <khuggler@uwyo.edu>
2023-04-14 13:20:30 -0600

    makeMarkdown commit

--- R/makeMarkdown.R
+++ R/makeMarkdown.R
@@ -16,6 +16,8 @@
 #' @export 
 makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
   
+ data(rf, package = "happybirthday")
+  
   plotdir<-paste0(tempdir, "/", "Plots")
   if(dir.exists(plotdir)){
     fls = dir(plotdir, full.names = TRUE, recursive = TRUE, include.dirs = TRUE)
@@ -43,7 +45,7 @@ makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
       fn <- paste(plotdir, fn, sep = "/")
       fn <- paste(fn, "png", sep = ".")
       png(filename = fn, height = 1400, width = 1500, res = 75)
-      par(mfrow = c(2, 2))
+      par(mfrow = c(3, 2))
       
       
       
@@ -63,17 +65,17 @@ makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
       }
      
       
-      lines(sub$t_, sub$mr3_mean, col = "red") # rolling quantile
+      lines(sub$t_, sub$mr48_mean, col = "red") # rolling quantile
       abline(h = mm, col = "blue", lty = 2)
       
       
       
       
       
-      # Home Range Plots--5 interval
+      # Home Range Plots--
       y1<-expression(Home ~ range ~ size ~ (km^2))
       plot(sub$t_, sub$HR, type = "l", ylab = y1,
-           xlab = "Date", main = "Home Range Size", cex = 1.25)
+           xlab = "Date", main = paste0("Home Range Size (", sub$Increment[1], ")"), cex = 1.25)
       
       tdiff<-round(48/max(sub$timediff, na.rm = T))
       if(nrow(sub)>tdiff){
@@ -86,29 +88,48 @@ makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
       }
       
       
-      lines(sub$t_, sub$hr3_mean, col = "red") # rolling quantile
+      lines(sub$t_, sub$hr48_mean, col = "red") # rolling quantile
       abline(h = mm, col = "blue", lty = 2)
       
       
       
       
       
-      # Residence Time
-      plot(sub$t_, sub$RT, type = "l", ylab = "Residence time (100 meters)",
-           xlab = "Date", main = "Residence time", cex = 1.25)
+      # Residence Time 100 meters
+      plot(sub$t_, sub$RT_100, type = "l", ylab = "Residence time (hours)",
+           xlab = "Date", main = "Residence time at 100 meters", cex = 1.25)
       
       tdiff<-round(48/max(sub$timediff, na.rm = T))
       if(nrow(sub)>tdiff){
-        mm <- quantile(sub[(nrow(sub) - tdiff):(nrow(sub)),]$RT,
+        mm <- quantile(sub[(nrow(sub) - tdiff):(nrow(sub)),]$RT_100,
                        probs = 0.75, na.rm = T)
         
       }else{ #'total quantile
-        mm <- quantile(sub$RT,
+        mm <- quantile(sub$RT_100,
                        probs = 0.75)
       }
       
       
-      lines(sub$t_, sub$rt3_mean, col = "red") # rolling quantile
+      lines(sub$t_, sub$rt48_mean_100, col = "red") # rolling quantile
+      abline(h = mm, col = "blue", lty = 2)
+      
+      
+      # Residence Time 500 meters
+      plot(sub$t_, sub$RT_500, type = "l", ylab = "Residence time (hours)",
+           xlab = "Date", main = "Residence time at 500 meters", cex = 1.25)
+      
+      tdiff<-round(48/max(sub$timediff, na.rm = T))
+      if(nrow(sub)>tdiff){
+        mm <- quantile(sub[(nrow(sub) - tdiff):(nrow(sub)),]$RT_500,
+                       probs = 0.75, na.rm = T)
+        
+      }else{ #'total quantile
+        mm <- quantile(sub$RT_500,
+                       probs = 0.75)
+      }
+      
+      
+      lines(sub$t_, sub$rt48_mean_500, col = "red") # rolling quantile
       abline(h = mm, col = "blue", lty = 2)
       
       
@@ -116,30 +137,35 @@ makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
       
       
       
-      # Visitation
-      plot(sub$t_, sub$visits, type = "l", ylab = "Number of visits (100 meters)",
+      # Visitation 100 meters
+      plot(sub$t_, sub$Vis_100, type = "l", ylab = "Number of visits (100 meters)",
            xlab = "Date", main = "Number of visits", cex = 1.25)
       
       tdiff<-round(48/max(sub$timediff, na.rm = T))
       if(nrow(sub)>tdiff){
-        mm <- quantile(sub[(nrow(sub) - tdiff):(nrow(sub)),]$visits,
+        mm <- quantile(sub[(nrow(sub) - tdiff):(nrow(sub)),]$Vis_100,
                        probs = 0.75, na.rm = T)
         
       }else{ #'total quantile
-        mm <- quantile(sub$visits,
+        mm <- quantile(sub$Vis_100,
                        probs = 0.75)
       }
       
       
-      lines(sub$t_, sub$vis3_mean, col = "red") # rolling quantile
+      lines(sub$t_, sub$vis48_mean_100, col = "red") # rolling quantile
       abline(h = mm, col = "blue", lty = 2)
       
       
+   
+    sub$PredictedProbability<-as.numeric(randomForest:::predict.randomForest(rf,sub,type='prob')[,1])
+    thresh<-1-0.018
+    plot(sub$t_, sub$PredictedProbability, type = "l", ylab = "Probability of parturition",
+         xlab = "Date", main = "Probability of Parturition", cex = 1.25, ylim = c(0, thresh))
+    abline(h = thresh, col = "blue", lty = 2)
+    
+   
       
-      
-      
-     
-      mf <- paste(paste("Ewe Frequency: ", id_df[id_df$AID == uni[k],]$Frequency,
+          mf <- paste(paste("Ewe Frequency: ", id_df[id_df$AID == uni[k],]$Frequency,
                           sep = " "),paste('AID: ',id_df[id_df$AID == uni[k],]$AID,sep=''),sep=' ')
      
       
@@ -151,7 +177,7 @@ makeMarkdown<-function(id_df, rollmean, subsetmonth, tempdir){
       
      
         vf <- paste("Tag Number:", id_df[id_df$AID == uni[k],]$VitFreq, sep = " ")
-        mtext(vf, font = 2, side = 3, line = -116, outer = T,
+        mtext(vf, font = 2, side = 3, line = -147, outer = T,
               cex = 2)
         
         dev.off()
