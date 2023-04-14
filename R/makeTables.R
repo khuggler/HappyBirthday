39dd5f91052438ba90999839f086d9c8de5e18b8
Katey <khuggler@uwyo.edu>
2023-04-14 13:51:29 -0600

    makeTables update

--- R/makeTables.R
+++ R/makeTables.R
@@ -1,26 +1,45 @@
-makeTables<-function(data, id_df, tempdir){
+#' @title makeTables
+#' @description create tables of most recent 12 locations
+#' @param gpsdat data.frame of gps data
+#' @param id_df data.frame of lookup table
+#' @param tempdir temporary directory to save files
+#' @return No return value. Function will save tables for rendering in rmarkdown
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[sp]{coordinates}}, \code{\link[sp]{is.projected}}, \code{\link[sp]{spTransform}}
+#' @rdname makeTables
+#' @export 
+#' @importFrom sp coordinates proj4string spTransform
+makeTables<-function(gpsdat, id_df, tempdir){
   
+  gpsdat<-gpsdat[complete.cases(gpsdat$x),]
   # reproject data to lat longs
 
-  sp::coordinates(data)<-c('x_', 'y_')
-  sp::proj4string(data)<-'+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
-  data<-sp::spTransform(data, '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+  sp::coordinates(gpsdat)<-c('x', 'y')
+  sp::proj4string(gpsdat)<-'+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+  data<-sp::spTransform(gpsdat, '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
   data<-data.frame(data)
   
   uni<-unique(data$AID)  
   
   all.locs<-data.frame()
   for(k in 1:length(uni)){
-      sub<-data[data$id == uni[k],]
-      sub<-sub[complete.cases(sub$t_),]
+      sub<-data[data$AID == uni[k],]
+      sub<-sub[complete.cases(sub$tdate),]
       
-      sub<-sub[order(sub$t_, decreasing = T),]
+      sub<-sub[order(sub$tdate, decreasing = T),]
       sub<-sub[1,]
       
       all.locs<-rbind(sub, all.locs)
     }
     
-    all.locs<-all.locs[, c('VitFreq', 'Frequency', 'SN', 'Species', 'IdCol', 'AID', 'Sex', 'x_', 'y_')]
+    all.locs<-all.locs[, c('VitFreq', 'Frequency', 'SN', 'Species', 'IdCol', 'AID', 'Sex', 'x', 'y')]
     
     tabby.file<-paste0(tempdir, "/Tables")
     if(dir.exists(tabby.file)){
@@ -35,13 +54,13 @@ makeTables<-function(data, id_df, tempdir){
     
     outs<-data.frame()
     for(i in 1:length(uni)){
-      sub<-data[data$id == uni[i],]
-      sub<-sub[complete.cases(sub$t_),]
-      sub<-sub[order(sub$t_, decreasing = T),]
+      sub<-data[data$AID == uni[i],]
+      sub<-sub[complete.cases(sub$tdate),]
+      sub<-sub[order(sub$tdate, decreasing = T),]
       
       sub<-sub[1:12,]
       
-      sub<-sub[, c('VitFreq', 'Frequency', 'SN', 'Species', 'IdCol', 'AID', 'Sex', 'x_', 'y_')]
+      sub<-sub[, c('VitFreq', 'Frequency', 'SN', 'Species', 'IdCol', 'AID', 'Sex', 'x', 'y')]
       
       outs<-rbind(outs, sub)
     }
